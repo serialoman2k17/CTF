084d7f867c9583c4efc0a87016077e2e2cffd137
serialoman2k17 <mister.smyshlyaev@bk.ru>
2021-04-22 16:54:48 +0700

    №10

--- Hard_clock.py
+++ Hard_clock.py
@@ -1,8 +1,5 @@
-def Hard_clock():
-    h = int(input())
-    m = int(input())
-    s = int(input())
+def Hard_clock(h, m, s):
     print(h * 30 + m * 30 / 60 + s * 30 / 3600)
 
 
-Hard_clock()
+Hard_clock(int(input('Enter hour: ')), int(input('Enter minutes: ')), int(input('Enter seconds: ')))
