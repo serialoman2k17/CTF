66d626c78c4795cdffc02a4e4e829fc47b4c2909
serialoman2k17 <mister.smyshlyaev@bk.ru>
2021-04-22 16:35:33 +0700

    №11

--- Clock.py
+++
@@ -1,9 +0,0 @@
-def clock():
-    a = int(input("Enter time: "))
-    days = a // (60 * 24)
-    hours = a % (60 * 24) // 60
-    minutes = a % 60
-    print(days, hours, minutes, sep=':')
-
-
-clock()
